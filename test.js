d32d5cb78d2e6b0b6f3bd1cae826f9eca3e10cd3
TureLindbladLaptop <ture.gustaf.lindblad@gmail.com>
2023-10-26 14:20:03 +0200

    Button now changes background color to red

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ document.getElementById("button").addEventListener('click', changeColor);
 
 function changeColor() {
     const element = document.getElementById("body");
-    const list = element.classList;
-    list.remove("bodyFlex")
+    element.style.backgroundColor("red");
     return alert("hej");
 }
